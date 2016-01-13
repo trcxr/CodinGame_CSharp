36ed61d8b48a590f8e4f5e8b0263f80e9e5729a7
Aman Ahuja <trcxr16@hotmail.com>
2016-01-13 23:55:16 +0530

    Added 2 more easy challenges
    Chuck Norris and MIME Type

---
+++ EASY/ChuckNorris.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        bool prevOne = true;
+        bool prevZero = true;
+        int one = 0;
+        int zero = 0;
+        string output = "";
+        string dummy = "";
+        string MESSAGE = Console.ReadLine();
+
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        for (int i = 0; i <= MESSAGE.Length; i++)
+        {
+            if (i == MESSAGE.Length)
+            {
+                if (one > 0)
+                {
+                    for (int x = 0; x < one; x++)
+                        dummy += "0";
+
+                    output += "0 " + dummy;
+                }
+                else if (zero > 0)
+                {
+                    for (int x = 0; x < zero; x++)
+                        dummy += "0";
+
+                    output += "00 " + dummy;
+                }
+
+            }
+            else
+            {
+                string binaryCharCode = Convert.ToString(MESSAGE[i], 2);
+
+                while (binaryCharCode.Length < 7)
+                {
+                    binaryCharCode = "0" + binaryCharCode;
+                }
+
+                for (int j = 0; j < binaryCharCode.Length; j++)
+                {
+
+                    if (binaryCharCode[j] == '1' && prevOne)
+                    {
+                        one++;
+                        prevOne = true;
+                        prevZero = false;
+                    }
+                    else if (binaryCharCode[j] == '0' && prevZero)
+                    {
+                        zero++;
+                        prevZero = true;
+                        prevOne = false;
+                    }
+                    else if (binaryCharCode[j] == '1' && !prevOne)
+                    {
+                        for (int x = 0; x < zero; x++)
+                            dummy += "0";
+
+                        output += "00 " + dummy + " ";
+                        dummy = "";
+                        zero = 0;
+                        one++;
+                        prevOne = true;
+                        prevZero = false;
+                    }
+                    else if (binaryCharCode[j] == '0' && !prevZero)
+                    {
+                        for (int x = 0; x < one; x++)
+                            dummy += "0";
+
+                        output += "0 " + dummy + " ";
+                        dummy = "";
+                        one = 0;
+                        zero++;
+                        prevOne = false;
+                        prevZero = true;
+                    }
+                }
+            }
+        }
+        Console.WriteLine(output);
+    }
+}
---
+++ EASY/MIMEType.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine()); // Number of elements which make up the association table.
+        int Q = int.Parse(Console.ReadLine()); // Number Q of file names to be analyzed.
+        Dictionary<string, string> d = new Dictionary<string, string>(
+            StringComparer.OrdinalIgnoreCase);  //Ignore case while checking keys
+
+        for (int i = 0; i < N; i++)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            d.Add(inputs[0], inputs[1]);         //Add to dictionary
+        }
+
+        for (int i = 0; i < Q; i++)
+        {
+            string FNAME = Console.ReadLine(); // One file name per line.
+            int index = FNAME.LastIndexOf('.'); //Check the first occurence of string or char
+                                                //from the end
+
+            int flag = -1;
+            if (index != -1)
+            {
+                if (d.ContainsKey(FNAME.Substring(index + 1)))
+                {
+                    string value = d[FNAME.Substring(index + 1)]; // returns value to the searched key
+                    Console.WriteLine(value);
+                    flag = 1;
+                }
+            }
+
+            if (flag == -1)
+            {
+                Console.WriteLine("UNKNOWN");
+            }
+        }
+    }
+}
